150291561d3ddaad2c5b949f62e44bfe1086db4e
salim <salimtukur1421@gmail.com>
2024-10-26 16:06:28 +0100

    Update tests.py

--- main/task/tests.py
+++ main/task/tests.py
@@ -1,11 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.urls import reverse, resolve
 from django.utils import timezone
 import datetime
 
 
 from .models import Task
+from .views import (
+    TaskListView, TaskCreateView,
+    TaskDeleteView, TaskDetailView,
+    TaskUpdateView
+)
 
 
 class TaskModelTest(TestCase):
@@ -25,3 +30,23 @@ class TaskModelTest(TestCase):
         self.assertIs(recent_task.was_recently_created(), False)
 
 
+class TaskUrlTest(SimpleTestCase):
+    def test_task_list_url(self):
+        url = reverse('task-list')
+        self.assertEqual(resolve(url).func.view_class, TaskListView)
+
+    def test_task_detail_url(self):
+        url = reverse('task-detail', args=['pk'])
+        self.assertEqual(resolve(url).func.view_class, TaskDetailView)
+
+    def test_task_create_url(self):
+        url = reverse('task-create')
+        self.assertEqual(resolve(url).func.view_class, TaskCreateView)
+
+    def test_task_update_url(self):
+        url = reverse('task-update', args=['pk'])
+        self.assertEqual(resolve(url).func.view_class, TaskUpdateView)
+
+    def test_task_delete_url(self):
+        url = reverse('task-delete', args=['pk'])
+        self.assertEqual(resolve(url).func.view_class, TaskDeleteView)
